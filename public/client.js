f40dcde09f6da89b8e40276e18299adcc73246dc
Shivansh Vikram Singh <116243866+ShivanshVikramSingh@users.noreply.github.com>
2023-07-26 20:51:29 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -3,8 +3,8 @@
     const form = document.getElementById("formm");
     const msgInp = document.getElementById("inpt");
     const container =document.getElementById("container");
-    const socket=new WebSocket("wss://sok.onrender.com")
-
+    //const socket=new WebSocket("wss://sok.onrender.com")
+const socket=io();
     const append =(message,position,namee)=>{
     const messageElement =document.createElement('div');
     if(position!=="center"){
