2e8f8c1c8d48d91ec97e7f94859772e0c38c15e9
Shivansh Vikram Singh <116243866+ShivanshVikramSingh@users.noreply.github.com>
2023-07-28 09:35:34 +0530

    Added Roo id for private messaging

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const server= require("http").createServer(app);
 const io= require("socket.io")(server)
 app.use(express.static(path.join(__dirname+"/public")))
 const users={}
-
-io.server.on('connection', socket=>{
-    socket.on('new-user-joined',namee=>{ 
-        users[socket.id]=namee;
-        socket.broadcast.emit('user-joined',namee);
+const conn={}
+io.sockets.on('connection', socket=>{
+    socket.on('new-user-joined',data=>{ 
+      socket.join(data.roomid);// Just added this for joining a particular room
+      console.log(data.roomid);
+        users[socket.id]=data.namee;
+        conn[socket.id]=data.roomid;
+        socket.to(data.roomid).emit('user-joined',data.namee);
     })   
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive', {message:message,name:users[socket.id]})
+    socket.on('send',(data)=>{
+        socket.to(data.roomid).emit('receive', {message:data.msg,name:data.namee/*users[socket.id]*/})
     })
-  socket.on('disconnect',message=>{
-    socket.broadcast.emit('left',users[socket.id]);
+  socket.on('disconnect',()=>{
+    socket.to(conn[socket.id]).emit('left',users[socket.id]);
     delete users[socket.id];
   })
 })
